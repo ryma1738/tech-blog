50470ece3e4c44226ff633cab61affad8d6142b6
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-08-14 13:11:05 -0600

    inital files

---
+++ server.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+require('dotenv').config();
+
+//backend server api code:
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// handlebars
+const hbs = handlebars.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//Sessions:
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
