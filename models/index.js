3b65bf645d437fcb6a021b3e08d914602c08cd74
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-08-17 19:25:30 -0600

    added comment and post models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,32 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
-module.exports = {User};
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+module.exports = {User, Post, Comment};
